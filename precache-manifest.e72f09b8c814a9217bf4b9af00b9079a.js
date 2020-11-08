950bcab174572f1337f7233547668973b9508296
DouglasMagno <domagn2@gmail.com>
2020-11-08 10:47:37 -0300

    gh-pages

---
+++ precache-manifest.e72f09b8c814a9217bf4b9af00b9079a.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7d6f0394a88f92e4bbe",
+    "url": "/profile/css/app.1bea99ac.css"
+  },
+  {
+    "revision": "d7d6f0394a88f92e4bbe",
+    "url": "/profile/js/app.0e9b4144.js"
+  },
+  {
+    "revision": "55bc4afb27fc9a0cf712",
+    "url": "/profile/js/chunk-52e4de86.8614a0ac.js"
+  },
+  {
+    "revision": "17b5290d97d2d8097e63",
+    "url": "/profile/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "17b5290d97d2d8097e63",
+    "url": "/profile/js/chunk-ebdaf33a.4c18f85c.js"
+  },
+  {
+    "revision": "bd45161c67e13cdf9467",
+    "url": "/profile/js/chunk-vendors.0080c295.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/profile/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/profile/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/profile/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/profile/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/profile/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/profile/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/profile/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/profile/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/profile/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "783dc4b97ec4997dfeef7576dbbf2816",
+    "url": "/profile/img/fontawesome-webfont.783dc4b9.svg"
+  },
+  {
+    "revision": "a3d1413304c5c8a379a97b3c9a628b03",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/profile/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/profile/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/profile/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/profile/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/profile/img/brand/white.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/profile/img/brand/favicon.png"
+  },
+  {
+    "revision": "31dcad846a6331abf2bbca01bd54241f",
+    "url": "/profile/img/brand/main.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/profile/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "dc89bbe7ec0d227e132b3385c7ad202d",
+    "url": "/profile/img/theme/ai.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/profile/img/theme/img-1-1200x1000-2.jpg"
+  },
+  {
+    "revision": "74c819c81298afe9ad22efa4d7b32f0a",
+    "url": "/profile/img/theme/frontend.jpg"
+  },
+  {
+    "revision": "effdbc87d0fcaa9d2745c6a4e496130e",
+    "url": "/profile/img/theme/backend-sec.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/profile/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/profile/img/theme/promo-1.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/profile/img/theme/profile.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/profile/img/theme/landing.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/profile/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "974dce46ea2d4f597adea063432ec71b",
+    "url": "/profile/img/theme/frontend-sec.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/profile/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/profile/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "ee8e0d497331292e2b152a3d50112f31",
+    "url": "/profile/img/theme/ai-2.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/profile/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "e9fef825f906d3cad12b4993225ea6a2",
+    "url": "/profile/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "240f3cc2211fce41972cc08167a8fb8b",
+    "url": "/profile/img/theme/backend.jpg"
+  }
+];
